6ae8aa464e1af77b94877941e2396315c8c62075
Abogoboga04 <135635987+Abogoboga04@users.noreply.github.com>
2025-06-25 17:32:22 +0700

    Update emojiquiz.py

--- cogs/emojiquiz.py
+++ cogs/emojiquiz.py
@@ -16,7 +16,7 @@ class EmojiQuiz(commands.Cog):
         self.questions = self.load_quiz_data()
         self.scores = {}  # Menyimpan skor peserta
 
-        self.game_channel_id = 1379458566452154438  # ID channel yang diizinkan
+        self.game_channel_id = 765140300145360896  # ID channel yang diizinkan
         self.bantuan_price = 35  # Harga bantuan
         self.reward_per_correct_answer = 30  # Hadiah per pertanyaan benar
         self.time_limit = 60  # Waktu batas untuk setiap pertanyaan
