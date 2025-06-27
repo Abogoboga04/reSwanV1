34e72435b32a657d4d664a7c88b929ca0325e954
Abogoboga04 <135635987+Abogoboga04@users.noreply.github.com>
2025-06-28 05:46:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -161,6 +161,7 @@ async def load_cogs():
         await bot.load_extension("cogs.emojiquiz")
         await bot.load_extension("cogs.hangman") 
         await bot.load_extension("cogs.quotes")
+        await bot.load_extension("cogs.newgame")
         print("âœ… Semua cogs berhasil dimuat.")
     except Exception as e:
         print(f"âŒ Gagal memuat cogs: {e}")
